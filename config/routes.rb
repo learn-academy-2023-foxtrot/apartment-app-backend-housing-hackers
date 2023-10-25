f36a738d52346348d2f39fb0df2f0ed0a4b1cde4
aesandino <anabellasandino@gmail.com>
2023-10-24 17:11:29 -0700

    keys

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   resources :apartments
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users,
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  path: '',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
